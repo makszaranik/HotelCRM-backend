27b37adebdaaa86f4a4b13000a2b8f2462736cd0
makszaranik <zaranikXZ@gmail.com>
2024-12-01 10:50:19 +0200

    remove models

--- models/userModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userShema = new mongoose.Shema({
-  username: {type: String, required: true, unique: true},
-  password: {type: String, required: true}
-})
-
-const user = mongoose.model('users', userShema)
-export default user
